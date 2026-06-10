e4cfc8251d01928e32dbad13e4705c39b0b501a5
danielmiceli85 <danielmiceli@gmail.com>
2026-06-10 12:57:11 -0300

    Desenvolvimento da consulta de produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -16,6 +18,10 @@ export class App {
     //Criando um objeto da biblioteca HttpClient
     httpClient = inject(HttpClient);
     
+    //Variável signal para armazenar a lista de produtos obtida da API
+    produtos = signal<any[]>([]);
+
+    
     //Atributo para capturar o nome do produto
     nomeProduto: string = '';
 
@@ -24,9 +30,11 @@ export class App {
 
       //Fazendo uma requisição HTTP GET para consultar os produtos na API
       this.httpClient
-        .get('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
+        .get<any[]>('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
         .subscribe((data) => { //Capturando a resposta da API
-            console.log(data); //Exibindo a resposta no console do navegador
+            //console.log(data); //Exibindo a resposta no console do navegador
+            //Armazenar os dados obtidos da API no signal
+            this.produtos.set(data);
         });
     }
 }
